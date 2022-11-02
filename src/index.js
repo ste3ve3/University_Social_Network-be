cbd68ed83e5b2fb50439fe957ca71c5ab0615f9d
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2022-11-02 09:24:25 -0700

    worked on chats

--- src/index.js
+++ src/index.js
@@ -18,6 +18,83 @@ import MemoryStore from "memorystore";
 import cookieParser from "cookie-parser";
 import socialMediaLoggedInUser from "./routes/socialMediaRoute.js";
 
+//chats
+import http from 'http';
+import { Server } from 'socket.io';
+import formatMessage from '../src/chat/messages.js';
+import {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} from '../src/chat/users.js';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+const botName = 'University Social Network Bot';
+
+// Run when client connects
+io.on('connection', (socket) => {
+  socket.on('joinRoom', ({ username }) => {
+    const user = userJoin(socket.id, username);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit(
+      'message',
+      formatMessage(botName, 'Welcome to the University Social Network Chat!')
+    );
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
 
 
 
@@ -32,6 +109,10 @@ const corsOptions = {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use('/chat', express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -73,7 +154,7 @@ mongoose.connection.once("open", ()=>{
 
 
 const port = process.env.PORT_NUMBER;
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`The server is running on ${port}`);
 })
 
